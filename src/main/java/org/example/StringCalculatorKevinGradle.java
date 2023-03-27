edcc75ff5adfde0065cf0fadc01e21df80bdddaa
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-03-27 17:12:50 +0200

    First commit with AssertThat and Pitest dependences

---
+++ src/main/java/org/example/StringCalculatorKevinGradle.java
@@ -0,0 +1,36 @@
+package org.example;
+
+        import org.apache.commons.lang3.StringUtils;
+
+        import java.util.Arrays;
+
+public class StringCalculatorKevinGradle {
+    public static Integer sum(String expression) {
+        String delimiter = ",";
+        final String customDelimiterPrefix = "//";
+        if (expression.startsWith(customDelimiterPrefix)) {
+            delimiter = getCustomDelimiter(expression);
+            expression = removeCustomDelimiter(expression);
+        }
+        return sumNumbers(expression, delimiter);
+    }
+
+    private static String getCustomDelimiter(String expression) {
+        final int customDelimiterPosition = 2;
+        return expression.split("")[customDelimiterPosition];
+    }
+
+    private static String removeCustomDelimiter(String expression) {
+        final int lastCustomDelimiterPosition = 4;
+        expression = expression.substring(lastCustomDelimiterPosition);
+        return expression;
+    }
+
+    private static Integer sumNumbers(String expression, String delimiter) {
+        return Arrays.stream(expression.split(delimiter))
+                .map(String::trim)
+                .filter(StringUtils::isNumeric)
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
+    }
+}
---
+++ src/test/java/org/example/StringCalculatorKevinGradleTest.java
@@ -0,0 +1,56 @@
+package org.example;
+
+        import org.junit.jupiter.api.Test;
+
+        import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * "" -> 0
+ * "1" -> 1
+ * "1,2" -> 3
+ * "1,2,3" -> 6
+ * "a" -> 0
+ * "1,a" -> 1
+ * "1,a,2" -> 3
+ * "1a, 2" -> 2
+ * "//#/3#2" -> 5
+ * "//#/3,2" -> 0"
+ * "//%/1%2%3" -> 6
+ */
+
+public class StringCalculatorKevinGradleTest {
+    @Test
+    void does_not_count_empty() {
+        assertThat(StringCalculatorKevinGradle.sum("")).isEqualTo(0);
+    }
+
+    @Test
+    void convert_text_in_to_number() {
+        assertThat(StringCalculatorKevinGradle.sum("1")).isEqualTo(1);
+    }
+
+    @Test
+    void sum_numbers() {
+        assertThat(StringCalculatorKevinGradle.sum("1,2")).isEqualTo(3);
+        assertThat(StringCalculatorKevinGradle.sum("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void ignore_not_numbers_on_sum() {
+        assertThat(StringCalculatorKevinGradle.sum("a")).isEqualTo(0);
+        assertThat(StringCalculatorKevinGradle.sum("1,a")).isEqualTo(1);
+        assertThat(StringCalculatorKevinGradle.sum("1,a,2")).isEqualTo(3);
+    }
+
+    @Test
+    void ignore_spaces_on_sum() {
+        assertThat(StringCalculatorKevinGradle.sum("2 2, 2,3 ")).isEqualTo(5);
+    }
+
+    @Test
+    void sum_with_custom_delimiter() {
+        assertThat(StringCalculatorKevinGradle.sum("//#/3#2")).isEqualTo(5);
+        assertThat(StringCalculatorKevinGradle.sum("//#/3,2")).isEqualTo(0);
+        assertThat(StringCalculatorKevinGradle.sum("//%/1%2%3")).isEqualTo(6);
+    }
+}
